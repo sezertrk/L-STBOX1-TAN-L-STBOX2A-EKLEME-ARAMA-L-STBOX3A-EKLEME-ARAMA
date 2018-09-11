057cafc53e89361311efe99d3100e88681bb9505
sezertrk <43032011+sezertrk@users.noreply.github.com>
2018-09-11 14:44:52 +0300

    Add files via upload

---
+++ LİSTBOX1 TAN LİSTBOX2A EKLEME ARAMA LİSTBOX3A EKLEME ARAMA/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int deger = -1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            deger = -1;
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (listBox1.Items[i].ToString() == textBox1.Text)
+                {
+                    deger = i;
+                }
+            }
+            
+            if (deger == -1)
+            {
+                MessageBox.Show("Girilen Değer Bulunamadı");
+            }
+            else
+            {
+                button2.Enabled = true;
+                button3.Enabled = true;
+
+            }
+
+
+        }  
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            listBox2.Items.Add(listBox1.Items[deger].ToString());
+            listBox1.Items.RemoveAt(deger);
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Add(listBox1.Items[deger].ToString());
+            listBox1.Items.RemoveAt(deger);
+        }
+    }
+}
